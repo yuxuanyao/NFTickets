6fa3816b68d30007596e897e2c25edf36848e89f
Yuxuan Yao <yuxuanyyx@gmail.com>
2022-01-15 19:11:22 -0500

    implemented circular seatmap template

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
-import Seatmap from './components/Seatmap.js';
+import Seatmap from './components/seatmap.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
       <Seatmap />
     </div>
   );
--- frontend/src/components/Seatmap.js
+++ frontend/src/components/Seatmap.js
@@ -1,12 +1,14 @@
 import React from 'react';
-
+import '../css/seatmap.css';
+import Seatmapcircle from './seatmap-circle';
 
 export default class Seatmap extends React.Component {
     render() {
         return (
             <div>
-                <p>Seatmapppp</p>
-            </div>
+                <Seatmapcircle />
+            </div >
+
         );
     }
 }
